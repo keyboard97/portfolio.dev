e2895b97fd3a8ed2cc96846b209cb774b2a7af41
Adrián Rodríguez <adrianrdguez87@gmail.com>
2026-06-17 17:32:53 +0200

    chore: update dependencies and configuration
    - Bump versions for @astrojs/check, @astrojs/react, and astro in package.json
    - Add autoprefixer to package.json and create postcss.config.mjs for PostCSS configuration
    - Replace ViewTransitions with ClientRouter in Layout.astro for improved routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://keyboard97.netlify.app",
-  integrations: [tailwind(), react()],
+  integrations: [react()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
